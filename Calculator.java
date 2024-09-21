154e9dcc355f65c6b126311a4824897e68a51828
mathiasddf <139523775+mathiasddf@users.noreply.github.com>
2024-09-21 13:49:50 -0500

    Update div

--- Calculator.java
+++ Calculator.java
@@ -20,6 +20,8 @@ public class Calculator {
     int mul(int a, int b){
         return a * b; 
     }
-    int div(int a, int b){ return 0; }
+    int div(int a, int b){ 
+        return a / b;    
+    }
     int mod(int a, int b){ return 0; }
 }
