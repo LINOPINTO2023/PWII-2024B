e2ae4660776a44fc5486ebcbfff9aa031c2e285f
Aarón Quiñonez <aquinonezd@unsa.edu.pe>
2024-09-21 12:40:06 -0500

    Método de multiplicación

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,7 @@
 public class Calculator {
     int add(int a, int b){ return 0; }
     int sub(int a, int b){ return 0; }
-    int mul(int a, int b){ return 0; }
+    int mul(int a, int b){ return a*b; }
     int div(int a, int b){ return 0; }
     int mod(int a, int b){ return 0; }
 }
