0d0c6d60e5f89ffebc03aa865fb829dc3d87ed4b
Aarón Quiñonez <aquinonezd@unsa.edu.pe>
2024-09-21 13:43:58 -0500

    Añadido método de multiplicación y entradas para primer número, segundo número y operador

--- Calculator.java
+++ Calculator.java
@@ -1,17 +1,25 @@
-
+import java.util.Scanner;
 public class Calculator {
-  public static void main(String [] args){
-    Scanner sc = new Scanner(System.in);
-    int primernum, segundonum;
-      
-    public int suma(int a, int b){ 
-        return a + b; 
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int primernum, segundonum, operador;
+        System.out.println("Ingrese el primer número");
+        primernum = sc.nextInt();
+        System.out.println("Ingrese el segundo número");
+        segundonum = sc.nextInt();
+        System.out.println("Ingrese el operador\n(1)Suma\n(2)Resta\n(3)Multiplicación\n(4)División");
+        operador = sc.nextInt();
+        
+        System.out.println("La suma es: "+ sum(primernum, segundonum));
     }
-    System.out.println("La suma es: "+ suma(primernum,segundonum));
     
+    public static int sum(int a, int b){ 
+        return a + b;
+    }
     int sub(int a, int b){ return 0; }
-    int mul(int a, int b){ return a*b; }
+    int mul(int a, int b){
+        return a * b; 
+    }
     int div(int a, int b){ return 0; }
     int mod(int a, int b){ return 0; }
-  }
 }
