7de6799eec2da2896456daab1c73f23862c95da5
DaniWik <bsenciaa@unsa.edu.pe>
2024-09-21 16:34:25 -0500

    Agregando metodo de suma y mensajes para ususario

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+class Calculator{
+    
+    //metodos para las operaciones de la calculadora
+    int add(int a, int b){
+        return a+b ;
+    }
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Calculator chistematicos = new Calculator();
+        
+        System.out.print("Escriba el primer número: ");
+        int a = sc.nextInt();
+
+        System.out.print("Escriba el segundo número: ");
+        int b = sc.nextInt();
+        
+        System.out.println("Escriba la operacion");
+        System.out.println("suma[+], resta[-], multiplicaion[*], division[/], modulo[%] ");
+        String operation = sc.next();
+
+        System.out.println("El resultado es");
+        //agregar logica para q imprima el resultado
+    }
+
+}
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Integrantes del grupo Chistematicos
 
+- Chávez Chambi, Marco David
 - Choquehuanca Bedoya, Denilson
 - Sencia Ale, Brian Daniel
 - Sivincha Machaca, Saul Andre
 - Yauli Merma, Diego Raul
-- Chávez Chambi, Marco David
