3424b4143c7c19bce7c985e8025de6c809e74ad7
Andres <aores@unsa.edu.pe>
2024-09-20 19:27:03 -0500

    Añadiendo la plantilla base

---
+++ Calculator.java
@@ -0,0 +1,8 @@
+
+public class Calculator {
+    int add(int a, int b){ return 0; }
+    int sub(int a, int b){ return 0; }
+    int mul(int a, int b){ return 0; }
+    int div(int a, int b){ return 0; }
+    int mod(int a, int b){ return 0; }
+}
