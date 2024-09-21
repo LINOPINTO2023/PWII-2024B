edba10a955892dbfe3796f4562fd160f93d6ba6e
AndersonArceValencia <aarceva@unsa.edu.pe>
2024-09-21 16:23:14 -0500

    Modifique el metodo mod y agregue un switch case para eligir el tipo de operacion

--- Calculator.java
+++ Calculator.java
@@ -7,21 +7,38 @@ public class Calculator {
         primernum = sc.nextInt();
         System.out.println("Ingrese el segundo número");
         segundonum = sc.nextInt();
-        System.out.println("Ingrese el operador\n(1)Suma\n(2)Resta\n(3)Multiplicación\n(4)División");
+        System.out.println("Ingrese el operador\n(1)Suma\n(2)Resta\n(3)Multiplicación\n(4)División"); //falta agregae mas operaciones
         operador = sc.nextInt();
-        
-        System.out.println("La suma es: "+ sum(primernum, segundonum));
+        switch (operador) {
+            case 1:
+            System.out.println("La suma es: "+ sum(primernum, segundonum));
+                break;
+            case 2:
+            System.out.println("La resta es: "+ sum(primernum, segundonum));//falta el metodo de resta
+            break;
+            case 3:
+            System.out.println("La multiplicacion es: "+ mul(primernum, segundonum));
+            break;
+            case 4:
+            System.out.println("La division es: "+ div(primernum, segundonum));
+            break;
+            default:
+            System.out.println("Instruccion Invalida");
+                break;
+        }
+       
     }
     
     public static int sum(int a, int b){ 
         return a + b;
     }
     int sub(int a, int b){ return 0; }
-    int mul(int a, int b){
+
+    public static int mul(int a, int b){
         return a * b; 
     }
-    int div(int a, int b){ 
+    public static int div(int a, int b){ 
         return a / b;    
     }
-    int mod(int a, int b){ return 0; }
+    int mod(int a, int b){ return a % b; }
 }
