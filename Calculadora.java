881f6de3eb69d3ff482370c1d2a2bdf118776d88
Milene Pacheco <mpachecoe@unsa.edu.pe>
2024-09-21 09:40:22 -0500

    metodo modulo

--- Calculadora.java
+++ Calculadora.java
@@ -13,7 +13,8 @@ class Calculadora{
                            "1: Suma\n"+
                            "2: Resta\n"+
                            "3: Multiplicacion\n"+
-                           "4: Division\n");
+                           "4: Division\n"+
+                           "5: Modulo\n");
         op = sc.nextInt();
         switch (op) {
             case 1: System.out.println("Resultado de la suma: "); break;
@@ -26,11 +27,21 @@ class Calculadora{
                  System.out.println("Error: No se puede dividir entre cero.");
                 }
                 break;
+            case 5:
+            if (num2 != 0) {
+                if (num2 != 0) {
+                    System.out.println("Resultado del módulo: " + modulo(num1, num2)); 
+                } else {
+                    System.out.println("Error: No se puede calcular el módulo con divisor cero.");
+                }
+            break;
         }
     }
 
     public static double division(double a, double b) {
         return a / b;
     }
-
+    public static double modulo(double a, double b) {
+        return a % b;
+    }
 }
