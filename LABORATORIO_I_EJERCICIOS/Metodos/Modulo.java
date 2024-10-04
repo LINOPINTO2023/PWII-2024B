6744ae732750b16c7d0a57bef3ed6d6e4f5ee909
maritza1818 <mmaronpu@unsa.edu.pe>
2024-10-04 17:15:54 -0500

    Subiendo avance del ejercicio 01 del Laboratorio

---
+++ LABORATORIO_I_EJERCICIOS/Metodos/Modulo.java
@@ -0,0 +1,14 @@
+package Metodos;
+
+/**
+ * Método para calcular el módulo de dos números.
+ * 
+ * @param num1 El primer número.
+ * @param num2 El segundo número.
+ * @return El resultado del módulo de num1 entre num2. Devuelve double.
+ */
+public class Modulo {
+    public static double modulo(double num1, double num2) {
+        return num1 % num2;
+    }
+}
---
+++ LABORATORIO_I_EJERCICIOS/Metodos/Resta.java
@@ -0,0 +1,14 @@
+package Metodos;
+
+/**
+ * Método para restar dos números.
+ * 
+ * @param num1 El primer número.
+ * @param num2 El segundo número.
+ * @return El resultado de restar ambos números. Devuelve double.
+ */
+public class Resta {
+    public static double restar(double num1, double num2) {
+        return num1 - num2;
+    }
+}
