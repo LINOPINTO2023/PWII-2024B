687103143a9e7fca391e6593030c064147debc1c
Anthony Caracela <139632579+AnthonyCaracela@users.noreply.github.com>
2024-10-04 17:20:04 -0500

    Modificando botones numéricos ahora aparecen en la celda y botón limpiar borra cualquier dato en la casilla

--- Lab03/bank.js
+++ Lab03/bank.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const botones = document.querySelectorAll(".numeros4");
-
+    const limpiarBtn = document.querySelector(".limpiar4");
+    const inputClave = document.querySelector(".texto4_2 + div input");
     // Generar un array de números del 0 al 9
     let numeros = Array.from({ length: 10 }, (_, i) => i);
 
@@ -10,5 +11,14 @@ document.addEventListener("DOMContentLoaded", function () {
     // Asignar los números aleatorios a los botones
     botones.forEach((boton, index) => {
         boton.textContent = numeros[index];
+        // Añadir el evento de click a cada botón numérico
+        boton.addEventListener("click", function () {
+            // Agregar el número del botón al input de clave
+            inputClave.value += boton.textContent;
+        });        
     });
+    // Limpiar el input cuando se presiona el botón "Limpiar"
+    limpiarBtn.addEventListener("click", function () {
+        inputClave.value = ''; // Borrar el contenido del input
+    });    
 });
