65b9f6a0198f678d0dbe871d8f41e02c582d8ca9
mathiasddf <139523775+mathiasddf@users.noreply.github.com>
2024-10-04 18:15:46 -0600

    palabras disponibles para adivinar y creacion de canvas para el manejo de lienzo

--- Lab03/Ejercicio3/ahorcado.js
+++ Lab03/Ejercicio3/ahorcado.js
@@ -3,3 +3,6 @@ let palabra;
 let palabraArray;
 let palabraOculta;
 let errores;
+const palabrasSimples = ["GATO", "PERRO", "CASA", "SOL", "LUNA", "AGUA"];
+const canvas = document.getElementById('ahorcadoCanvas');
+const ctx = canvas.getContext('2d'); /*LIENZO-DIBUJO*/
