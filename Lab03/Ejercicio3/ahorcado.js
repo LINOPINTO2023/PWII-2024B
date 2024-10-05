65fc755eb8fb1f0c2f666b247daed67b80506b0f
mathiasddf <139523775+mathiasddf@users.noreply.github.com>
2024-10-04 18:18:56 -0600

    uso de math.random  para que se eliga una de las opciones a adivinar y creacion de un array vacio con "_" para adivinar la palabra

--- Lab03/Ejercicio3/ahorcado.js
+++ Lab03/Ejercicio3/ahorcado.js
@@ -9,4 +9,13 @@ const ctx = canvas.getContext('2d'); /*LIENZO-DIBUJO*/
 const palabraHTML = document.getElementById('palabra');
 const teclado = document.getElementById('teclado');
 const botonReiniciar = document.getElementById('botonReiniciar');
-const mensajeFinal = document.getElementById('mensajeFinal'); 
+const mensajeFinal = document.getElementById('mensajeFinal');
+
+// Función para inicializar o reiniciar el juego
+function inicializarJuego() {
+        palabra = palabrasSimples[Math.floor(Math.random() * palabrasSimples.length)];
+        palabraArray = palabra.split(''); //USO DE SPLIT PARA CONVERTIR EN UN ARRAY DE CHARS
+        palabraOculta = Array(palabra.length).fill('_');/*Se crea un nuevo array vacío con la misma longitud que la palabra seleccionada. 
+            Luego, fill('_') llena ese array con guiones bajos (_)*/
+        errores = 0;
+}
