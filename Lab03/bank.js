866edfc133110ba8ed5113bf22b8cedf7404f7f4
Andres <aores@unsa.edu.pe>
2024-10-05 09:39:35 -0500

    Mejorando la pagina de banco(tildes, apariencia y validacion de clave de 6 digitos)

--- Lab03/bank.js
+++ Lab03/bank.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     const botones = document.querySelectorAll(".numeros4");
     const limpiarBtn = document.querySelector(".limpiar4");
-    const inputClave = document.querySelector(".texto4_2 + div input");
+    const inputClave = document.getElementById("area_clave");
+
     // Generar un array de números del 0 al 9
     let numeros = Array.from({ length: 10 }, (_, i) => i);
 
@@ -13,12 +14,21 @@ document.addEventListener("DOMContentLoaded", function () {
         boton.textContent = numeros[index];
         // Añadir el evento de click a cada botón numérico
         boton.addEventListener("click", function () {
-            // Agregar el número del botón al input de clave
-            inputClave.value += boton.textContent;
-        });        
+            // Limitar a 6 caracteres
+            if (inputClave.value.length < 6) {
+                inputClave.value += boton.textContent;
+            }
+        });
     });
+
     // Limpiar el input cuando se presiona el botón "Limpiar"
     limpiarBtn.addEventListener("click", function () {
         inputClave.value = ''; // Borrar el contenido del input
-    });    
+    });
+
+    inputClave.addEventListener("input", function () {
+        if (this.value.length > 6) {
+            this.value = this.value.slice(0, 6);
+        }
+    });
 });
