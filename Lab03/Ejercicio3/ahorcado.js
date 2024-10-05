6eb6121cee9917ebc12b424894c95d0a60f657ff
mathiasddf <139523775+mathiasddf@users.noreply.github.com>
2024-10-04 18:16:49 -0600

    creacion de un pequeño teclado , para manejar las letras acertadas; un boton de reinicio y mensaje de derrota o victoria

--- Lab03/Ejercicio3/ahorcado.js
+++ Lab03/Ejercicio3/ahorcado.js
@@ -6,3 +6,7 @@ let errores;
 const palabrasSimples = ["GATO", "PERRO", "CASA", "SOL", "LUNA", "AGUA"];
 const canvas = document.getElementById('ahorcadoCanvas');
 const ctx = canvas.getContext('2d'); /*LIENZO-DIBUJO*/
+const palabraHTML = document.getElementById('palabra');
+const teclado = document.getElementById('teclado');
+const botonReiniciar = document.getElementById('botonReiniciar');
+const mensajeFinal = document.getElementById('mensajeFinal'); 
