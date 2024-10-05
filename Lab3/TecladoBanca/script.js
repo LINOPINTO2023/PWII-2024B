2ffae70fd1cf86a66fe62646e07a9afe73c24606
JP-777 <jp777.pro@gmail.com>
2024-10-04 19:44:21 -0500

    Last Changes

--- Lab3/TecladoBanca/script.js
+++ Lab3/TecladoBanca/script.js
@@ -1,11 +1,25 @@
 const buttons = document.querySelectorAll(".button");
-const numbers = [];
+const passwordField = document.getElementById("clave-internet");
+const clearButton = document.getElementById("clear");
+
+let numbers = Array.from({ length: 10 }, (_, i) => i);
+
+numbers = numbers.sort(() => Math.random() - 0.5);
+
+buttons.forEach((button, index) => {
+    button.innerHTML = numbers[index];
+});
 
 buttons.forEach(button => {
-  let num;
-  do {
-    num = Math.floor(Math.random() * 10) + 1;
-  } while (numbers.includes(num));
-  numbers.push(num);
-  button.innerHTML = num;
+    button.addEventListener('click', () => {
+        if (button.textContent === "Limpiar") {
+            passwordField.value = "";
+        } else {
+            passwordField.value += button.textContent;
+        }
+    });
+});
+
+clearButton.addEventListener('click', () => {
+    passwordField.value = "";
 });
