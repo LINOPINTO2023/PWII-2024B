3260185247d01c8ee3777db6b0caf401eea6d22f
Christian Casso <ccasso@unsa.edu.pe>
2024-10-05 23:52:10 -0500

    Mejorando el Ejercicio 1

--- lab03/Ejercicio_1/index.js
+++ lab03/Ejercicio_1/index.js
@@ -1,7 +1,9 @@
 const buttonContainer = document.getElementById("buttonContainer");
 
 function CrearBotones() {
-  const inputClave = document.querySelector(".columnContent input");
+  const inputClave = document.querySelector(
+    ".columnContent input[type='number']"
+  );
   const numeros = [...Array(10).keys()];
 
   numeros.sort(() => Math.random() - 0.5);
@@ -17,14 +19,58 @@ function CrearBotones() {
 
     buttonContainer.appendChild(boton);
   });
+
   const limpiar = document.createElement("button");
   limpiar.textContent = "LIMPIAR";
   limpiar.className = "button limpiar";
 
   limpiar.addEventListener("click", () => {
     inputClave.value = "";
+    clearMessages();
   });
 
   buttonContainer.appendChild(limpiar);
 }
+
 CrearBotones();
+
+const ingresarButton = document.querySelector("button[type='button']");
+const inputNumeroTarjeta = document.querySelector(
+  ".contentInput input[type='number']"
+);
+const inputTipoDocumento = document.querySelectorAll(".contentInput select")[1];
+const inputClave = document.querySelector(
+  ".columnContent input[type='number']"
+);
+const messageContainer = document.createElement("div");
+messageContainer.className = "message-container";
+document.querySelector(".columnContent").appendChild(messageContainer);
+
+function clearMessages() {
+  messageContainer.innerHTML = "";
+}
+
+ingresarButton.addEventListener("click", () => {
+  clearMessages();
+
+  const numeroTarjeta = inputNumeroTarjeta.value;
+  const tipoDocumento = inputTipoDocumento.value;
+  const clave = inputClave.value;
+
+  if (!numeroTarjeta || !tipoDocumento || !clave) {
+    const errorMessage = document.createElement("p");
+    errorMessage.textContent = "Por favor, llena todos los datos requeridos.";
+    errorMessage.style.color = "red";
+    messageContainer.appendChild(errorMessage);
+  } else if (clave.length !== 6) {
+    const errorMessage = document.createElement("p");
+    errorMessage.textContent = "Por favor, ingresa 6 dígitos en la clave.";
+    errorMessage.style.color = "red";
+    messageContainer.appendChild(errorMessage);
+  } else {
+    const successMessage = document.createElement("p");
+    successMessage.textContent = "Ingreso exitoso.";
+    successMessage.style.color = "green";
+    messageContainer.appendChild(successMessage);
+  }
+});
