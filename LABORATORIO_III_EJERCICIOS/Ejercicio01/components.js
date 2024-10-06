de98f2ed1b567ddc9f8b553db26b8b8b7d932203
maritza1818 <mmaronpu@unsa.edu.pe>
2024-10-05 22:38:51 -0500

    Corregido error de sintaxis en el script que causaba problemas de funcionamiento. Reestructure las rutas de las imágenes para organizarlas en una carpeta 'img', mejorando así la organización de mi proyecto.

--- LABORATORIO_III_EJERCICIOS/Ejercicio01/components.js
+++ LABORATORIO_III_EJERCICIOS/Ejercicio01/components.js
@@ -1,26 +1,29 @@
 
-window.onload = function () {
+window.onload = function() {
+ 
     const buttons = document.querySelectorAll(".casilla");
-
+    
     const numbers = Array.from({ length: 10 }, (_, i) => i).sort(() => Math.random() - 0.5);
-
+    
+  
     buttons.forEach((button, index) => {
-        button.textContent = numbers[index];
+        button.textContent = numbers[index]; 
         button.addEventListener('click', (event) => {
-            event.preventDefault();
-            agregarNumero(numbers[index]); /
+            event.preventDefault(); 
+            agregarNumero(numbers[index]); 
         });
     });
 };
 
 function agregarNumero(numero) {
-    const passwordField = document.getElementById("clave-internet");
-    if (passwordField.value.length < 6) {
-        passwordField.value += numero;
+    const passwordField = document.getElementById("clave-internet"); 
+    if (passwordField.value.length < 6) { 
+        passwordField.value += numero; 
     }
 }
 
+
 function limpiarFormulario() {
     const passwordField = document.getElementById("clave-internet");
-    passwordField.value = "";
+    passwordField.value = ""; 
 }
