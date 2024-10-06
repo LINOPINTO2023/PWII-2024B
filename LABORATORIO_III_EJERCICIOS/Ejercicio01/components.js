eeba1a3f9d79c2c66bf306415a3ab1c035a7fb66
maritza1818 <mmaronpu@unsa.edu.pe>
2024-10-05 22:06:21 -0500

    fix: evitar que el formulario se envíe al hacer clic en los botones del teclado virtual
    - Se agregó 'event.preventDefault()' a los eventos de clic de los botones del teclado para evitar la recarga de la página.
    
    - Ahora, los números se agregan correctamente al campo de contraseña sin que el formulario se envíe.

--- LABORATORIO_III_EJERCICIOS/Ejercicio01/components.js
+++ LABORATORIO_III_EJERCICIOS/Ejercicio01/components.js
@@ -1,21 +1,26 @@
-window.onload = function() {
+
+window.onload = function () {
     const buttons = document.querySelectorAll(".casilla");
+
     const numbers = Array.from({ length: 10 }, (_, i) => i).sort(() => Math.random() - 0.5);
 
     buttons.forEach((button, index) => {
         button.textContent = numbers[index];
-        button.onclick = () => agregarNumero(numbers[index]); 
+        button.addEventListener('click', (event) => {
+            event.preventDefault();
+            agregarNumero(numbers[index]); /
+        });
     });
 };
 
 function agregarNumero(numero) {
     const passwordField = document.getElementById("clave-internet");
-    if (passwordField.value.length < 6) { 
-        passwordField.value += numero; 
+    if (passwordField.value.length < 6) {
+        passwordField.value += numero;
     }
 }
 
 function limpiarFormulario() {
     const passwordField = document.getElementById("clave-internet");
-    passwordField.value = ""; 
+    passwordField.value = "";
 }
