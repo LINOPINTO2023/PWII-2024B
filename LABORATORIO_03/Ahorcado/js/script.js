db6b5dc60aa9ae7820e040777a99a35cb48c9381
MMaraP <dmara@unsa.edu.pe>
2024-10-07 17:53:43 -0500

    generando los dibujos

--- LABORATORIO_03/Ahorcado/js/script.js
+++ LABORATORIO_03/Ahorcado/js/script.js
@@ -41,4 +41,61 @@ function adivinarLetra() {
         intentos++;
         dibujarAhorcado(intentos);
     }
+    
+function dibujarAhorcado(intentos) {
+    const cuadro = document.getElementById('cuadro');
+    const dibujar = cuadro.getContext('2d');
+
+    if (intentos === 1) {
+        dibujar.beginPath();
+        dibujar.moveTo(50, 350);
+        dibujar.lineTo(150, 350);
+        dibujar.stroke();
+    } else if (intentos === 2) {
+        dibujar.beginPath();
+        dibujar.moveTo(100, 350);
+        dibujar.lineTo(100, 50);
+        dibujar.stroke();
+    } else if (intentos === 3) {
+        dibujar.beginPath();
+        dibujar.moveTo(100, 50);
+        dibujar.lineTo(250, 50);
+        dibujar.stroke();
+    } else if (intentos === 4) {
+        dibujar.beginPath();
+        dibujar.moveTo(250, 50);
+        dibujar.lineTo(250, 100);
+        dibujar.stroke();
+    } else if (intentos === 5) {
+        dibujar.beginPath();
+        dibujar.arc(250, 130, 30, 0, Math.PI * 2);
+        dibujar.stroke();
+    } else if (intentos === 6) {
+        dibujar.beginPath();
+        dibujar.moveTo(250, 160);
+        dibujar.lineTo(250, 250);
+        dibujar.stroke();
+    } else if (intentos === 7) {
+        dibujar.beginPath();
+        dibujar.moveTo(250, 200);
+        dibujar.lineTo(220, 180);
+        dibujar.stroke();
+    } else if (intentos === 8) {
+        dibujar.beginPath();
+        dibujar.moveTo(250, 200);
+        dibujar.lineTo(280, 180);
+        dibujar.stroke();
+    } else if (intentos === 9) {
+        dibujar.beginPath();
+        dibujar.moveTo(250, 250);
+        dibujar.lineTo(220, 300);
+        dibujar.stroke();
+    } else if (intentos === 10) {
+        dibujar.beginPath();
+        dibujar.moveTo(250, 250);
+        dibujar.lineTo(280, 300);
+        dibujar.stroke();
+    }
+}
+
 }
