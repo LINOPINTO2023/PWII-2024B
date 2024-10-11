43bfd4dc0e19b7813b3c5fb01787c0d90f70e50e
MMaraP <dmara@unsa.edu.pe>
2024-10-11 12:42:54 -0500

    Funcion menu para el ejercicio

---
+++ LABORATORIO_04/MARA MAMANI, DONNY MOISES/Ejercicio_Python.py
@@ -0,0 +1,10 @@
+def menu():
+    print("MENU DE OPCIONES")
+    print("INGRESAR DATOS DE VENTAS")
+    print("VER RESULTADOS DE VENTAS")
+    print("SALIR")
+
+#Para controlar la parte del codigo a ejecutar
+if __name__ == "__main__":
+    menu()
+    
