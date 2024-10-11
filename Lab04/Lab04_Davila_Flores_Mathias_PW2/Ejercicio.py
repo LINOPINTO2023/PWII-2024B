9402da208fdf99c7e6b553f3468c7f5a7ba79f17
mathiasddf <mdavilaf@unsa.edu.pe>
2024-10-11 16:23:57 -0500

    Creacion del bucle general para el manejo del menu y la creacion del menu

--- Lab04/Lab04_Davila_Flores_Mathias_PW2/Ejercicio.py
+++ Lab04/Lab04_Davila_Flores_Mathias_PW2/Ejercicio.py
@@ -9,3 +9,18 @@ acumulado_importe_ventas = 0  # Total de ventas
 ventas_femeninas = 0  # Ventas de clientes femeninos con importes entre 140 y 1000
 ventas_rango = 0  # Ventas con importes entre 70 y 500
 tipo_cliente = 0
+while verificar_menu:
+    print("\nMENU DE OPCIONES\n[1] REGISTRAR VENTA DE PASAJE\n[2] REPORTAR VENTAS\n[3] SALIR")
+    opcion = int(input("Seleccione una opción: "))
+    
+    if opcion == 1:
+        
+
+    elif opcion == 2:
+        
+
+    elif opcion == 3:
+        verificar_menu = False
+
+    else:
+        print("ERROR: Opción no válida. Intente de nuevo.")
