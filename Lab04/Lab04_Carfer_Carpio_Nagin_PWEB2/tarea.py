4634e2e81d7aba89621eb55510dc793a82f31555
Jazmin <140028251+NaginCarferC@users.noreply.github.com>
2024-10-11 18:21:03 -0500

    bucle del menú y opciones

--- Lab04/Lab04_Carfer_Carpio_Nagin_PWEB2/tarea.py
+++ Lab04/Lab04_Carfer_Carpio_Nagin_PWEB2/tarea.py
@@ -6,3 +6,22 @@ importnetofem140 = 0
 acumulimportventas = 0
 importnetotipo1 = 0
 importpromtipo1 = 0
+# Bucle infinito para mantener el menú activo
+while menu:
+    print("Menú:")
+    print("[1] Registrar venta de pasaje")
+    print("[2] Reportar ventas")
+    print("[3] Salir")
+    opcion = input("Elija una opción: ")
+    # Esta es la pción para registrar venta de pasajes
+    if opcion == "1":
+
+      
+    elif opcion == "2":
+        # Ventas
+
+    # Opción para salir del programa
+    elif opcion == "3":
+        menu=False
+    else:
+        print("Opción no válida. Elija opcion 1, 2 o 3")
