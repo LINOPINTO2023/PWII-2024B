9ff83af849395b3373a897782cb32b7089ad6ef8
mathiasddf <mdavilaf@unsa.edu.pe>
2024-10-11 16:26:38 -0500

    contenido para el manejo de la pcion 1 (uso de do-while, validaciones de entradas de datos, calculos, actu de datos para la opcion 2, resultados de la venta y menu de dos opciones

--- Lab04/Lab04_Davila_Flores_Mathias_PW2/Ejercicio.py
+++ Lab04/Lab04_Davila_Flores_Mathias_PW2/Ejercicio.py
@@ -14,7 +14,78 @@ while verificar_menu:
     opcion = int(input("Seleccione una opción: "))
     
     if opcion == 1:
-        
+        while True:
+            # Validar tipo de cliente
+            tipo_cliente = int(input("Ingrese tipo de cliente (1 o 2): "))
+            while tipo_cliente != 1 and tipo_cliente != 2:
+                print("Error. Ingrese una opcion valida")
+                tipo_cliente = int(input("Ingrese tipo de cliente (1 o 2): "))
+
+            # Validar cantidad de pasajes
+            numero_pasajes = int(input("Ingrese cantidad de pasajes: "))
+            while numero_pasajes <= 0:
+                print("Error. Ingrese una opcion valida")
+                numero_pasajes = int(input("Ingrese cantidad de pasajes: "))
+            
+            # Validar género del cliente
+            genero_cliente = input("Ingrese género del cliente (M/F): ").upper()
+            while genero_cliente != 'M' and genero_cliente != 'F':
+                print("Error. Ingrese una opcion valida")
+                genero_cliente = input("Ingrese género del cliente (M/F): ").upper()
+            
+            # Validar tipo de servicio
+            tipo_servicio = int(input("Ingrese tipo de servicio (1: Económico, 2: Ejecutivo, 3: Primera Clase): "))
+            while tipo_servicio != 1 and tipo_servicio != 2 and tipo_servicio != 3:
+                print("Error. Ingrese una opcion valida")
+                tipo_servicio = int(input("Ingrese tipo de servicio (1: Económico, 2: Ejecutivo, 3: Primera Clase): "))
+            
+            # Precios
+            if tipo_servicio == 1:
+                precio = 70.00
+            elif tipo_servicio == 2:
+                precio = 140.00
+            elif tipo_servicio == 3:
+                precio = 280.00
+
+            # Descuentos segun el  numero de pasajes
+            if numero_pasajes == 1:
+                desc = 0.00
+            elif 2 <= numero_pasajes <= 5:
+                desc = 0.05
+            elif 6 <= numero_pasajes <= 10:
+                desc = 0.12
+            else:
+                desc = 0.15
+            
+            # Cálculos de importes
+            importe_bruto = numero_pasajes * precio
+            monto_descuento = importe_bruto * desc
+            importe_neto = importe_bruto - monto_descuento
+
+            # Actualización de datos
+            acumulado_importe_ventas += importe_neto
+
+            if genero_cliente == 'M':
+                genero_m += 1
+            if genero_cliente == 'F' and 140 <= importe_neto <= 1000:
+                ventas_femeninas += 1
+            if 70 <= importe_neto <= 500:
+                ventas_rango += 1
+            if tipo_cliente == 1:
+                tipo_cliente_1 += 1
+                acumulado_importe_neto_tipo_1 += importe_neto
+            
+            # Mostrar resultados de la venta
+            print("\nResumen de la venta:")
+            print(f"Importe Bruto: ${importe_bruto:.2f}")
+            print(f"Descuento aplicado: ${monto_descuento:.2f}")
+            print(f"Importe Neto: ${importe_neto:.2f}\n")
+            
+            # Menu para ver si desea realizar otro movimiento o salir de la opcion 1 (registrar pasaje de venta)
+            print("\nMENU DE OPCIONES\n[1] REGISTRAR OTRA VENTA\n[2] SALIR")
+            otra_opcion = int(input("Seleccione una opción: "))
+            if otra_opcion == 2:
+                break  # Salir al menú principal
 
     elif opcion == 2:
         
