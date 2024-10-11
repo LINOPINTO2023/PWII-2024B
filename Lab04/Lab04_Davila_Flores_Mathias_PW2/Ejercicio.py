f95ef68cdf347d564f48f992c416c630111c3307
mathiasddf <mdavilaf@unsa.edu.pe>
2024-10-11 16:22:20 -0500

    creacion de variables para el manejo de datos numericos para la opcion 1 y 2 del menu asi como la creacion de la variable que verifique el menu para manejar el bucle general

---
+++ Lab04/Lab04_Davila_Flores_Mathias_PW2/Ejercicio.py
@@ -0,0 +1,11 @@
+verificar_menu = True
+genero_m = 0  # Cantidad de clientes masculinos
+importe_bruto = 0
+importe_neto = 0
+monto_descuento = 0
+tipo_cliente_1 = 0  # Contador de clientes de tipo 1
+acumulado_importe_neto_tipo_1 = 0
+acumulado_importe_ventas = 0  # Total de ventas
+ventas_femeninas = 0  # Ventas de clientes femeninos con importes entre 140 y 1000
+ventas_rango = 0  # Ventas con importes entre 70 y 500
+tipo_cliente = 0
