7b9e68a0a6d59e4d8c42fd4f79d5f35373c4e2d5
Nikole Salas <nsalasi@unsa.edu.pe>
2024-10-12 04:37:42 +0000

    obtener elementos del DOM

---
+++ LABORATORIO III SALAS_IDME_NIKOLE/Ejercicio 1/script_ejercicio_01.js
@@ -0,0 +1,6 @@
+const keyboard = document.getElementById('virtualKeyboard');
+const pinInput = document.getElementById('pinInput');
+const captchaImageElement = document.getElementById('captchaImage');
+const captchaInputElement = document.getElementById('captchaInput');
+const refreshCaptchaButton = document.getElementById('refreshCaptcha');
+const submitButton = document.getElementById('submitButton');
