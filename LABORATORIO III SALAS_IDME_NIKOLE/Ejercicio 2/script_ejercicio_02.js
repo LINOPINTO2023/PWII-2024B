2c41142a39464ec410e3e61e34dec7874d8664ef
Nikole Salas <nsalasi@unsa.edu.pe>
2024-10-12 04:21:29 +0000

    definir variables y evento de clic

--- LABORATORIO III SALAS_IDME_NIKOLE/Ejercicio 2/script_ejercicio_02.js
+++ LABORATORIO III SALAS_IDME_NIKOLE/Ejercicio 2/script_ejercicio_02.js
@@ -2,3 +2,22 @@
 const display = document.getElementById('display');
 const buttons = document.getElementById('buttons');
 const historyList = document.getElementById('historyList');
+let currentExpression = '';
+let displayExpression = '';
+const operationStack = [];
+
+buttons.addEventListener('click', event => {
+    const target = event.target;
+    if (target.matches('button')) {
+        const action = target.dataset.action;
+        const value = target.dataset.num;
+
+        if (action) {
+            handleAction(action);
+        } else if (value) {
+            appendToExpression(value);
+        }
+
+        updateDisplay();
+    }
+});
