07a36309a0f329f0fef9c5009cd9a9cb2c042f6d
Nikole Salas <nsalasi@unsa.edu.pe>
2024-10-12 04:38:09 +0000

    definir teclas y captcha

--- LABORATORIO III SALAS_IDME_NIKOLE/Ejercicio 1/script_ejercicio_01.js
+++ LABORATORIO III SALAS_IDME_NIKOLE/Ejercicio 1/script_ejercicio_01.js
@@ -4,3 +4,13 @@ const captchaImageElement = document.getElementById('captchaImage');
 const captchaInputElement = document.getElementById('captchaInput');
 const refreshCaptchaButton = document.getElementById('refreshCaptcha');
 const submitButton = document.getElementById('submitButton');
+const keys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'C', '0', '⌫'];
+const captchaImages = [
+    { src: 'captcha1.png', text: 'Prueba1' },
+    { src: 'captcha2.png', text: 'Test' },
+    { src: 'captcha3.png', text: 'Imagen3' },
+    { src: 'captcha4.png', text: 'esuntest' },
+    { src: 'captcha5.png', text: '123' }
+];
+const captchaFolder = 'captcha-images/';
+let currentCaptchaText = '';
