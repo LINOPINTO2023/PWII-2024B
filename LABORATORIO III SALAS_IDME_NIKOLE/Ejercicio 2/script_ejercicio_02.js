01a2f9ac4fe40a4576bf701d288ac5c68248b76d
Nikole Salas <nsalasi@unsa.edu.pe>
2024-10-12 04:20:41 +0000

    obtener elementos del DOM

---
+++ LABORATORIO III SALAS_IDME_NIKOLE/Ejercicio 2/script_ejercicio_02.js
@@ -0,0 +1,4 @@
+// Elementos del DOM
+const display = document.getElementById('display');
+const buttons = document.getElementById('buttons');
+const historyList = document.getElementById('historyList');
