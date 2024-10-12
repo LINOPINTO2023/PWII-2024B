4309c6da621dbe29ef6825d550b294439fd13e8c
Nikole Salas <nsalasi@unsa.edu.pe>
2024-10-12 04:40:55 +0000

    arreglar errores

--- LABORATORIO III SALAS_IDME_NIKOLE/Ejercicio 1/script_ejercicio_01.js
+++ LABORATORIO III SALAS_IDME_NIKOLE/Ejercicio 1/script_ejercicio_01.js
@@ -1,10 +1,7 @@
 const keyboard = document.getElementById('virtualKeyboard');
 const pinInput = document.getElementById('pinInput');
-const captchaImageElement = document.getElementById('captchaImage');
-const captchaInputElement = document.getElementById('captchaInput');
-const refreshCaptchaButton = document.getElementById('refreshCaptcha');
-const submitButton = document.getElementById('submitButton');
 const keys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'C', '0', '⌫'];
+
 const captchaImages = [
     { src: 'captcha1.png', text: 'Prueba1' },
     { src: 'captcha2.png', text: 'Test' },
@@ -14,12 +11,19 @@ const captchaImages = [
 ];
 const captchaFolder = 'captcha-images/';
 let currentCaptchaText = '';
+const captchaImageElement = document.getElementById('captchaImage');
+const captchaInputElement = document.getElementById('captchaInput');
+const refreshCaptchaButton = document.getElementById('refreshCaptcha');
+const submitButton = document.getElementById('submitButton');
+
+// Función para obtener una imagen CAPTCHA aleatoria
 function getRandomCaptcha() {
     const randomIndex = Math.floor(Math.random() * captchaImages.length);
     const selectedCaptcha = captchaImages[randomIndex];
     currentCaptchaText = selectedCaptcha.text;
     return captchaFolder + selectedCaptcha.src;
 }
+
 function updateCaptcha() {
     captchaImageElement.src = getRandomCaptcha();
 }
@@ -29,6 +33,7 @@ refreshCaptchaButton.addEventListener('click', updateCaptcha);
 document.addEventListener('DOMContentLoaded', () => {
     updateCaptcha();
 });
+
 function validateCaptcha() {
     const userCaptchaInput = captchaInputElement.value.trim();
 
@@ -45,6 +50,7 @@ submitButton.addEventListener('click', (e) => {
     e.preventDefault();
     validateCaptcha();
 });
+
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -63,14 +69,15 @@ function renderKeyboard() {
         keyboard.appendChild(button);
     });
 }
+
 function handleKeyPress(key) {
     if (key === 'C') {
-      pinInput.value = '';
+        pinInput.value = '';
     } else if (key === '⌫') {
-      pinInput.value = pinInput.value.slice(0, -1);
+        pinInput.value = pinInput.value.slice(0, -1);
     } else if (pinInput.value.length < 6) {
-      pinInput.value += key;
+        pinInput.value += key;
     }
-  }
-  
-  renderKeyboard();
+}
+
+renderKeyboard();
