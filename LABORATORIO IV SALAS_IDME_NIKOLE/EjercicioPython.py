17baa977319e8f52abd357fd9115a77801559a64
Nikole Salas <nsalasi@unsa.edu.pe>
2024-10-13 04:42:39 +0000

    Agregar cálculo de precio y descuentos java

--- LABORATORIO IV SALAS_IDME_NIKOLE/EjercicioPython.py
+++ LABORATORIO IV SALAS_IDME_NIKOLE/EjercicioPython.py
@@ -4,3 +4,30 @@ def ingresar_datos():
     genero = input("Ingrese el género del cliente (M/F): ").lower()
     tipoServicio = int(input("Ingrese el tipo de servicio (1-Económica, 2-Ejecutiva, 3-Primera clase): "))
     return tipoCliente, cantPasajes, genero, tipoServicio
+    def calcular_precio(cantPasajes, tipoServicio):
+    # Tipo de servicio
+    if tipoServicio == 1:
+        precio = 70
+    elif tipoServicio == 2:
+        precio = 140
+    elif tipoServicio == 3:
+        precio = 280
+    else:
+        print("Tipo de servicio inválido.")
+        return 0, 0, 0
+    
+    # Descuento por cantidad de pasajes
+    if cantPasajes == 1:
+        descuento = 0
+    elif 2 <= cantPasajes <= 5:
+        descuento = 0.05
+    elif 6 <= cantPasajes <= 10:
+        descuento = 0.12
+    else:
+        descuento = 0.15
+
+    # Operaciones para calcular importes
+    importeBruto = cantPasajes * precio
+    montoDescuento = importeBruto * descuento
+    importeNeto = importeBruto - montoDescuento
+    return importeBruto, montoDescuento, importeNeto
