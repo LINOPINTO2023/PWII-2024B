7e7dfd2931bf8f922086ac25f88a308dca85a7d5
MMaraP <dmara@unsa.edu.pe>
2024-10-18 15:55:26 -0500

    Ejercicio resuelto

--- LABORATORIO_05/src/EJERCICIO_AJEDREZ/Ejercicio2b.py
+++ LABORATORIO_05/src/EJERCICIO_AJEDREZ/Ejercicio2b.py
@@ -1,2 +1,11 @@
 from interpreter import draw
 from chessPictures import *
+from picture import Picture
+from colors import *
+
+pieza = knight
+dupla = Picture.join(pieza, pieza.negative())
+tablero = Picture.under(dupla, dupla.verticalMirror())
+
+
+draw(tablero)
