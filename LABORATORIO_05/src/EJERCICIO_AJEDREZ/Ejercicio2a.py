71797e53cbfe45a9210a60c762b7ca7619d2b9fd
MMaraP <dmara@unsa.edu.pe>
2024-10-18 15:05:23 -0500

    probando la funcion

--- LABORATORIO_05/src/EJERCICIO_AJEDREZ/Ejercicio2a.py
+++ LABORATORIO_05/src/EJERCICIO_AJEDREZ/Ejercicio2a.py
@@ -4,5 +4,5 @@ from chessPictures import *
 #pruebas de picture
 pieza = knight
 pieza2 = queen
-piezaUp = Picture.under(pieza, pieza2)
-draw(piezaUp)
+piezaRepeat = Picture.verticalRepeat(pieza, 6)
+draw(piezaRepeat)
