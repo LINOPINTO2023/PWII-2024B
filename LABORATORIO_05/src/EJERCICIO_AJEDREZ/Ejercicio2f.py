33c1d12bd813af8255670c04816a4e12b6464a40
MMaraP <dmara@unsa.edu.pe>
2024-10-18 15:58:24 -0500

    Ejercicio F resuelto

--- LABORATORIO_05/src/EJERCICIO_AJEDREZ/Ejercicio2f.py
+++ LABORATORIO_05/src/EJERCICIO_AJEDREZ/Ejercicio2f.py
@@ -1,2 +1,12 @@
 from interpreter import draw
 from chessPictures import *
+from picture import Picture
+from colors import *
+
+casilla = square
+dupla = Picture.join(casilla, casilla.negative())
+tablero = Picture.horizontalRepeat(dupla, 4)
+tablero2 = Picture.negative(tablero)
+tablero3 = Picture.under(tablero, tablero2)
+
+draw(tablero3.verticalRepeat(2))
