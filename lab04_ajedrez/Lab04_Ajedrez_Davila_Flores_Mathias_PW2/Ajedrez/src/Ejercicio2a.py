af4371cdc31b674c67171cb0c7bbbe1db12a1fe7
mathiasddf <mdavilaf@unsa.edu.pe>
2024-10-18 18:17:37 -0500

    ejercicio a

--- lab04_ajedrez/Lab04_Ajedrez_Davila_Flores_Mathias_PW2/Ajedrez/src/Ejercicio2a.py
+++ lab04_ajedrez/Lab04_Ajedrez_Davila_Flores_Mathias_PW2/Ajedrez/src/Ejercicio2a.py
@@ -1,3 +1,12 @@
 from interpreter import draw
 from chessPictures import *
 
+horse_white = knight
+horse_black = horse_white.negative()
+
+row_1 = horse_white.join(horse_black)
+row_2 = row_1.negative()
+
+board = row_1.under(row_2)
+
+draw(board) 
