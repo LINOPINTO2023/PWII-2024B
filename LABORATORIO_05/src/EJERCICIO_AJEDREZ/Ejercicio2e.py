7979dfe88b2e51017219f6f5e5988896c1f49808
MMaraP <dmara@unsa.edu.pe>
2024-10-18 15:57:30 -0500

    Ejerciico E resuelto

--- LABORATORIO_05/src/EJERCICIO_AJEDREZ/Ejercicio2e.py
+++ LABORATORIO_05/src/EJERCICIO_AJEDREZ/Ejercicio2e.py
@@ -1,2 +1,10 @@
 from interpreter import draw
 from chessPictures import *
+from picture import Picture
+from colors import *
+
+casilla = square
+dupla = Picture.join(casilla.negative(), casilla)
+tablero = Picture.horizontalRepeat(dupla, 4)
+
+draw(tablero)
