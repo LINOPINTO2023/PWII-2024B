18d661345b4f65cfbebfb671a45df6046ef5b382
Andres <aores@unsa.edu.pe>
2024-10-17 19:50:14 -0500

    try

---
+++ lab04/ejercicio.py
@@ -0,0 +1,12 @@
+def esEscalar(m):
+    d = m[0][0]
+    for i in range(len(m)):
+        for j in range(len(m)):
+            if i != j:
+                if m[i][j] != 0:
+                    print(m[i][j])
+                    return False
+            elif m[i][j] != d:
+                print(m[i][j])
+                return False
+    return True
