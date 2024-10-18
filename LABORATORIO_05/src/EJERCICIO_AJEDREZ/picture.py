cbc35fd28e2e9223203bfbc74d62a79ba1ac270b
AntonyTapia <atapiahu@unsa.edu.pe>
2024-10-18 14:45:30 -0500

    funcion para voltear la pieza horizontalmente

--- LABORATORIO_05/src/EJERCICIO_AJEDREZ/picture.py
+++ LABORATORIO_05/src/EJERCICIO_AJEDREZ/picture.py
@@ -18,3 +18,10 @@ class Picture:
       for value in self.img:
           vertical.append(value[::-1])
       return Picture(vertical)
+  
+  def horizontalMirror(self):
+    """ Devuelve el espejo horizontal de la imagen """
+    horizontal = []
+    for fila in self.img:
+        horizontal.insert(0, fila)  # Agrega cada fila al inicio de la nueva lista
+    return Picture(horizontal)
