4d2d4bbdfbad5538526a1de059fb7e4ff59c03c5
Andres <aores@unsa.edu.pe>
2024-10-18 14:21:45 -0500

    Añadiendo mejoras

--- lab04_ajedrez/Lab04_Ore_Soto_Andres_Raul/ejercicio1.py
+++ lab04_ajedrez/Lab04_Ore_Soto_Andres_Raul/ejercicio1.py
@@ -1,4 +1,6 @@
-from archivos_ajedrez import *
-from archivos_ajedrez.chessPictures import rock
+from archivos_ajedrez.chessPictures import *
 from archivos_ajedrez.interpreter import draw
-draw(rock)
+from archivos_ajedrez.picture import *
+
+# Crea una instancia de Picture con la imagen rock
+draw(square._invColor(GRAY))
