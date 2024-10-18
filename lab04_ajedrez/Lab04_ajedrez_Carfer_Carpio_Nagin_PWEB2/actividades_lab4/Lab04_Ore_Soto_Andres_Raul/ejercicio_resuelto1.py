353b8c2283ef733bd7a5fd03c6984c93f473ffbd
Andres <aores@unsa.edu.pe>
2024-10-18 11:17:44 -0500

    añadiendo ejercicios resueltos

---
+++ lab04_ajedrez/Lab04_ajedrez_Carfer_Carpio_Nagin_PWEB2/actividades_lab4/Lab04_Ore_Soto_Andres_Raul/ejercicio_resuelto1.py
@@ -0,0 +1,16 @@
+def esEscalar(m):
+    d = m[0][0]
+    for i in range(len(m)):
+        for j in range(len(m)):
+            if i != j:
+                if m[i][j] != 0:
+                    print(m[i][j])
+                    return False
+            elif m[i][j] != d:
+                print(m[i][j])
+                return False
+    return True
+
+
+def esUnitaria(m):
+    return m[0][0] == 1 and esEscalar(m)
