697b9a496d1689dd6e6f4b70511b2b0c6a2ecaca
MMaraP <dmara@unsa.edu.pe>
2024-10-18 15:56:12 -0500

    ejercicio C resuelto

--- LABORATORIO_05/src/EJERCICIO_AJEDREZ/Ejercicio2c.py
+++ LABORATORIO_05/src/EJERCICIO_AJEDREZ/Ejercicio2c.py
@@ -1,2 +1,10 @@
 from interpreter import draw
 from chessPictures import *
+from picture import Picture
+from colors import *
+
+pieza = queen
+tablero = Picture.horizontalRepeat(pieza, 4)
+
+
+draw(tablero)
