b1073b02145bb9e7e9a0cac65b3627e9c4620441
maritza1818 <mmaronpu@unsa.edu.pe>
2024-10-17 19:48:51 -0500

    Verificacion de si una matriz es escalar o no ademas se implemento comentarios que indiquen los casos como matrices escalares y no escalares como casos de prueba

--- LabOficial_04/LaboratorioIVOficial_Marron_Puma_Maritza/Actividades/actividad_01.py
+++ LabOficial_04/LaboratorioIVOficial_Marron_Puma_Maritza/Actividades/actividad_01.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 def esEscalar(m):
   d = m[0][0]
   for i in range(len(m)):
@@ -6,8 +6,22 @@ def esEscalar(m):
       if i != j:
         if m[i][j] != 0:
           print(m[i][j])
+          print(f"Elemento fuera del alcance de la diagonal superior e inferior: {m[i][j]}")
           return False
       elif m[i][j] != d:
+          
         print(m[i][j])
+        print(f"Elemento en el alcance de la diagonal superior e inferior es diferente : {m[i][j]}")
         return False
+  print("La matriz es escalar")
   return True
+matriz_escalar1 = np.array([[3, 0, 0],
+                           [0, 3, 0],
+                           [0, 0, 3]])
+matriz_noescalar2 = np.array([[3, 0, 0],
+                           [0, 2, 0],
+                           [0, 0, 3]])
+print("Resultados sera matriz escalar:")
+esEscalar(matriz_escalar1)
+print("Resultados sera matriz escalar:")
+esEscalar(matriz_noescalar2)
