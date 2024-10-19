697ff38aa7ff1ffea078cb83dea797cc4cab639d
SaulS. <ssivincham@unsa.edu.pe>
2024-10-18 19:42:31 -0500

    solucionando

--- Laboratorio4Ajedrez/SenciaAleBryanDaniel/src/Ejercicio2g.py
+++ Laboratorio4Ajedrez/SenciaAleBryanDaniel/src/Ejercicio2g.py
@@ -2,7 +2,7 @@ from interpreter import draw
 from chessPictures import *
 
 primBlancas = (rock.under(square.negative())).join(knight.under(square)).join(bishop.under(square.negative())) 
-repetBlancas = (bishop.under(square.negative())).join(knight.under(square)).join(rock.under(square.negative()))
+repetBlancas = (bishop.under(square)).join(knight.under(square.negative)).join(rock.under(square))
 filaBlancas = primBlancas.join(queen.under(square)).join(king.under(square.negative())).join(repetBlancas)
 filaNegras = filaBlancas.negative()
 filapeonesBlancos = (pawn.under(square)).join(pawn.under(square.negative())).horizontalRepeat(4)
--- Laboratorio4Ajedrez/SivinchaMachacaSaulAndre/src/Ejercicio2g.py
+++ Laboratorio4Ajedrez/SivinchaMachacaSaulAndre/src/Ejercicio2g.py
@@ -1,16 +1,13 @@
 from interpreter import draw
 from chessPictures import *
 
-
 firstWhiteRow = (rock.under(square.negative())).join(knight.under(square)).join(bishop.under(square.negative()))
-repeatWhiteRow = (bishop.under(square.negative())).join(knight.under(square)).join(rock.under(square.negative()))
+repeatWhiteRow = (bishop.under(square)).join(knight.under(square.negative())).join(rock.under(square))
 whitePiecesRow = firstWhiteRow.join(queen.under(square)).join(king.under(square.negative())).join(repeatWhiteRow)
 blackPiecesRow = whitePiecesRow.negative()
 whitePawnsRow = (pawn.under(square)).join(pawn.under(square.negative())).horizontalRepeat(4)
 blackPawnsRow = whitePawnsRow.negative()
 emptyRow = (square.join(square.negative())).horizontalRepeat(4)
 emptyRows = emptyRow.up(emptyRow.negative()).verticalRepeat(2)
-
 chessBoard = blackPiecesRow.up(blackPawnsRow).up(emptyRows).up(whitePawnsRow).up(whitePiecesRow)
-
 draw(chessBoard)
