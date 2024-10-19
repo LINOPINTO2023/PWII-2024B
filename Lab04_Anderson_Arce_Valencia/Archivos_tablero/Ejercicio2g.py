4d5edec3cd7780c61fdb53ae35afbb19a6e23045
AndersonArceValencia <aarceva@unsa.edu.pe>
2024-10-19 14:33:06 -0500

    He podido hacer el tablero en una sola linea de codigo, pero aun falta poner las fichas de ajedrez, creo que tendre que hacer un metodo superponer

--- Lab04_Anderson_Arce_Valencia/Archivos_tablero/Ejercicio2g.py
+++ Lab04_Anderson_Arce_Valencia/Archivos_tablero/Ejercicio2g.py
@@ -1,2 +1,5 @@
 from interpreter import draw
 from chessPictures import *
+
+tablero = square.under(square.negative()).join(square.negative().under(square)).horizontalRepeat(4).verticalRepeat(4)
+draw(tablero)
