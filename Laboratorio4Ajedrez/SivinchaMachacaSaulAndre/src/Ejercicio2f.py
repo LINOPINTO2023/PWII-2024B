96d59363b4c5aa016973526ebe610c8cd1fb63a5
SaulS. <ssivincham@unsa.edu.pe>
2024-10-18 19:05:13 -0500

    Correction in class Picture and finishing all the exercises

--- Laboratorio4Ajedrez/SivinchaMachacaSaulAndre/src/Ejercicio2f.py
+++ Laboratorio4Ajedrez/SivinchaMachacaSaulAndre/src/Ejercicio2f.py
@@ -1,2 +1,10 @@
 from interpreter import draw
 from chessPictures import *
+
+neg = square.negative()
+row = square.join(neg).join(square).join(neg)
+row = row.join(row)
+
+negativeRow = row.negative()
+
+draw(row.up(negativeRow).verticalRepeat(2))
