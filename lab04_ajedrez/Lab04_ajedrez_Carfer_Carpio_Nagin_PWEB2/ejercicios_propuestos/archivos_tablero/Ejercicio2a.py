bc6e59f37f8c62f4e69546ad8a1be714d5d1dade
Nagin <ncarferc@unsa.edu.pe>
2024-10-19 16:45:06 -0500

    Añadiendo ejercicio A

--- lab04_ajedrez/Lab04_ajedrez_Carfer_Carpio_Nagin_PWEB2/ejercicios_propuestos/archivos_tablero/Ejercicio2a.py
+++ lab04_ajedrez/Lab04_ajedrez_Carfer_Carpio_Nagin_PWEB2/ejercicios_propuestos/archivos_tablero/Ejercicio2a.py
@@ -1,2 +1,14 @@
 from interpreter import draw
 from chessPictures import *
+# Aqui la primera fila, caballo blanco y caballo negro
+row1 = knight.negative().join(knight)
+
+
+# Aqui la segunda fila, caballo negro y caballo blanco
+row2 = knight.join(knight.negative())
+
+# aqui unimos las anteriores filas verticalmente
+row3= row2.under(row1)
+
+# Dibujamos, ademas de haber puesto la dimension de 120 x 120 para que se acomode al dibujo
+draw(row3, 120, 120)
