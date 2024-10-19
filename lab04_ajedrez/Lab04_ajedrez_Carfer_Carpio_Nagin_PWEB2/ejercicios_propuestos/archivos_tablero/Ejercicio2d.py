ad77d933470d9a5215a0425a31d437880a0c6e51
Nagin <ncarferc@unsa.edu.pe>
2024-10-19 16:54:23 -0500

    Añadiendo ejercicio D

--- lab04_ajedrez/Lab04_ajedrez_Carfer_Carpio_Nagin_PWEB2/ejercicios_propuestos/archivos_tablero/Ejercicio2d.py
+++ lab04_ajedrez/Lab04_ajedrez_Carfer_Carpio_Nagin_PWEB2/ejercicios_propuestos/archivos_tablero/Ejercicio2d.py
@@ -1,2 +1,6 @@
 from interpreter import draw
 from chessPictures import *
+#Fila de cuadrados blancos y negros, primera forma
+row= square.join(square.negative()).horizontalRepeat(4)
+
+draw(row, 463, 58)
