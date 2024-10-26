fca6f139e876deef6e98091f9711f9e3159deb81
maritza1818 <maritzaclaudiamarronpuma@gmail.com>
2024-10-26 18:37:31 -0500

    Implement word guessing game with random letter hiding

--- LABORATORIOV_MARRONPUMAMARITZAA/src/game.py
+++ LABORATORIOV_MARRONPUMAMARITZAA/src/game.py
@@ -1,63 +1,10 @@
 import random
+
 words = ["meow", "cats", "claudita", "aea"]
 word = random.choice(words)
 guessed_letters = []
-while True:
-    letter = input("Ingrese su letra correspondiente: ")
-    guessed_letters.append(letter)
-    hidden_letter = ''
-    for c in word:
-        if c in guessed_letters:
-            hidden_letter += c
-        else:
-            hidden_letter += "_"
-    print(hidden_letter)
-    if "_" not in hidden_letter:
-        print("¡Ganaste el juego!")
-        break
 attempts = 5
 
-while attempts > 0:
-    hidden_letter = ''.join([c if c in guessed_letters else '_' for c in word])
-    print("Palabra:", hidden_letter)
-    print("Intentos restantes:", attempts)
-    
-    letter = input("Ingrese su letra correspondiente: ").lower()
-    guessed_letters.append(letter)
-    if letter not in word:
-        attempts -= 1
-    
-    if "_" not in hidden_letter:
-        print("¡Ganaste el juego!")
-        break
-
-if attempts == 0:
-    print("La palabra era:", word)
-while attempts > 0:
-    hidden_letter = ''.join([c if c in guessed_letters else '_' for c in word])
-    print("Palabra:", hidden_letter)
-    print("Intentos restantes:", attempts)
-
-    letter = input("Ingrese una letra o la palabra completa: ").lower()
-    
-    if len(letter) == 1:
-        guessed_letters.append(letter)
-        if letter not in word:
-            attempts -= 1
-    elif len(letter) == len(word):
-        if letter == word:
-            print("Adivinaste la palabra:", word)
-            break
-        else:
-            attempts -= 1
-            print("La palabra no es correcta.")
-    
-    if '_' not in hidden_letter:
-        print("Adivinaste la palabra:", word)
-        break
-
-if attempts == 0:
-    print("La palabra era:", word)
 def hide_letters(word):
     num_letters_to_hide = max(1, int(len(word) * 0.6))
     hidden_word = list(word)
@@ -68,3 +15,31 @@ def hide_letters(word):
 
 hidden_word_start = hide_letters(word)
 print("Palabra inicial:", hidden_word_start)
+
+while attempts > 0:
+    hidden_letter = ''.join([c if c in guessed_letters else '_' for c in word])
+    print("Palabra:", hidden_letter)
+    print("Intentos restantes:", attempts)
+
+    letter = input("Ingrese una letra o la palabra completa: ").lower()
+
+    if len(letter) == 1 and letter.isalpha():
+        guessed_letters.append(letter)
+        if letter not in word:
+            attempts -= 1
+    elif len(letter) == len(word):
+        if letter == word:
+            print("¡Ganaste! Adivinaste la palabra:", word)
+            break
+        else:
+            attempts -= 1
+            print("Intento fallido. La palabra no es correcta.")
+    else:
+        print("Entrada inválida. Debes ingresar una letra o intentar adivinar la palabra completa.")
+
+    if '_' not in hidden_letter:
+        print("¡Felicidades! Adivinaste la palabra:", word)
+        break
+
+if attempts == 0:
+    print("Perdiste. La palabra era:", word)
