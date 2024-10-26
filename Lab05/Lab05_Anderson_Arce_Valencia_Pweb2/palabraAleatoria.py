ae518f5bd4e40083d4dd419bac3eb951cb644548
AndersonArceValencia <aarceva@unsa.edu.pe>
2024-10-26 17:09:52 -0500

    Ya lo he validado y ahora es completamente funcional, incluso con doble felicitacion xd

--- Lab05/Lab05_Anderson_Arce_Valencia_Pweb2/palabraAleatoria.py
+++ Lab05/Lab05_Anderson_Arce_Valencia_Pweb2/palabraAleatoria.py
@@ -1,13 +1,11 @@
 import random
-
+#Ocultar la palñabra
 words = ["Tonto", "Jordan", "Desconocido"]
 n = random.choice(words).lower()
 num_letras = len(n)
 max_ocultas = int(num_letras * 0.6)  # Máximo 60% de letras ocultas
 indices_ocultos = random.sample(range(num_letras), max_ocultas)  # Elegimos qué letras ocultar
-print(max_ocultas)
-print(indices_ocultos)
-print(n)
+
 palabra_oculta = ""
 
 for i, letra in enumerate(n):
@@ -17,18 +15,16 @@ for i, letra in enumerate(n):
         palabra_oculta += letra
 
 
-
 #juegp:
 intentos = 5
 while intentos > 0 and "_" in palabra_oculta:
     print("Palabra:"," ".join(palabra_oculta))
-    print("-----------------------------------------")
-    acertar = input("Introduce una letra o la palabra completa: " ).lower()
-    print(palabra_oculta)
+    acertar = input(f"Introduce una letra o la palabra completa: ""\n------------------------------------------: " ).lower()
     #Primero un condicional para determinar si gano introducciendo la palabra completa
     if len(acertar) == 1 or len(acertar) == len(n):
         if acertar == n:
-            print(f"Felicidades usted ha adivinado la palabra: {n}")
+            print(f"Felicidades usted ha adivinado la palabra")
+            palabra_oculta = n
             break
     
     #Segundo, si introduce una sola Letra
@@ -41,6 +37,7 @@ while intentos > 0 and "_" in palabra_oculta:
                         palabran += palabra_oculta[i]
                 palabra_oculta = palabran
                 print("Encontraste una letra")
+                print("-----------------------------------------")
         else:
             intentos -= 1
             print(f"Intento incorrecto. Te quedan {intentos} intentos.")
@@ -48,6 +45,6 @@ while intentos > 0 and "_" in palabra_oculta:
         print("Entrada invalida, debes introduccir una letra o palabra de " , len(n), "letras")
 
 if '_' not in palabra_oculta:
-    print(f"Ganaste, La palabra era:{n}")
+    print(f"Ganaste!!!!!!!!\nLa palabra era:{n}")
 else:
     print("Perdiste :p")
