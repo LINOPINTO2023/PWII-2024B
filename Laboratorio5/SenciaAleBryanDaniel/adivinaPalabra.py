6725e8e5c6742d779f029a558b5bc34e1396233c
DaniWik <bsenciaa@unsa.edu.pe>
2024-10-26 16:11:57 -0500

    lista e estructura del programa

---
+++ Laboratorio5/SenciaAleBryanDaniel/adivinaPalabra.py
@@ -0,0 +1,10 @@
+
+palabras = ["PYTHON", "UNSA", "SISTEMAS", "CONFIGURACION", "VIERNES","ADIVINAR"]
+
+def ocultar_letras():
+    return 
+
+def jugar():
+    return
+#ejecucion del juego XD
+jugar()
