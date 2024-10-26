19b9433eaee553f0d301d9308486b84ee95ce832
Jazmin <140028251+NaginCarferC@users.noreply.github.com>
2024-10-26 16:26:54 -0500

    funcion para generar pistas(letras) aleatorias

--- Lab05/lab05_Carfer_Carpio_Nagin_Jazmin/ahorcado.py
+++ Lab05/lab05_Carfer_Carpio_Nagin_Jazmin/ahorcado.py
@@ -4,3 +4,17 @@ import random
 listapalabras = ['elefante', 'lapicero', 'hormiga', 'gallina', 'ave', 'antro', 'azul', 'piña',
            'caracol', 'raton', 'gorila', 'peaton', 'tenedor', 'pintura', 'blanco',
            'iguana', 'leon', 'gato', 'gallo', 'comida', 'televisor', 'papel', 'lonchera']
+def generadorLetrasVisibles(palabra):
+    # Aqui calculamos cuántas letras se deben mostrar, no más del 40%
+    letrasTotal = len(palabra)
+    maximoVisible = int(letrasTotal * 0.4)
+    cantidadMostrar = random.randint(1, maximoVisible)
+    
+    # Seleccionamos posiciones aleatorias para mostrar
+    posiciones = random.sample(range(letrasTotal), cantidadMostrar)
+    # Construye la palabra oculta
+    palabraOculta = list('_' * letrasTotal)
+    for posicion in posiciones:
+        palabraOculta[posicion] = palabra[posicion]
+    
+    return ''.join(palabraOculta)
