034b9babcd4e15e0049f70c672c81040a0e917fd
mathiasddf <mdavilaf@unsa.edu.pe>
2024-10-26 13:08:46 -0500

    Funcion para ocultar palabras (usando cantidad de letras, lo maximo que se puede ocultar y que indices ocultar de manera aleatoria sin repetir

--- Lab05/Lab05_Davila_Flores_Mathias_PW2/adivinar.py
+++ Lab05/Lab05_Davila_Flores_Mathias_PW2/adivinar.py
@@ -2,3 +2,9 @@ import random
 
 # Lista de palabras 
 palabras = ["algoritmo", "codigo", "programa", "hardware", "software", "redes", "base", "datos", "bug", "compilador"]
+
+#Funcion para ocultar las letras 
+def ocultar_letras(palabra):
+    num_letras = len(palabra)
+    max_ocultas = int(num_letras * 0.6)  # Máximo 60% de letras ocultas
+    indices_ocultos = random.sample(range(num_letras), max_ocultas)  # Elegimos qué letras ocultar
