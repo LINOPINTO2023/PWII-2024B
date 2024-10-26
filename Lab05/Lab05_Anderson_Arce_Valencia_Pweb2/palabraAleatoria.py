36dcaf5db6021e74b92e6e1b523dbf4671ccceec
JP-777 <jparedessai@unsa.edu.pe>
2024-10-25 19:36:50 -0500

    Tratar de imprimir la palabra em oculto

--- Lab05/Lab05_Anderson_Arce_Valencia_Pweb2/palabraAleatoria.py
+++ Lab05/Lab05_Anderson_Arce_Valencia_Pweb2/palabraAleatoria.py
@@ -2,7 +2,13 @@ import random
 
 words = ["Tonto", "Jordan","Desconocido"]
 n = random.choice(words)
-i = 1
-while i < 4:
-    print("Palabra:","".join(n))
-    i= i + 1
+i = 5
+aciertos = 0
+while i >= 0:
+    for _ in n:
+       poculto= n.replace("-")
+    print("Palabra:"," ".join(n))
+    print("Intentos restantes: ", i,)
+    print("-----------------------------------------")
+    i = i - 1
+    print(n)
