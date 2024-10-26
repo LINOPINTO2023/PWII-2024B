972e1f96cc555ac8441ab1968a91d052e5377de8
mathiasddf <mdavilaf@unsa.edu.pe>
2024-10-26 13:16:37 -0500

    while que verifica la longitud de la letra o palabra , que tiene como maximo la cantidad de letras de la palabra que se adivinara

--- Lab05/Lab05_Davila_Flores_Mathias_PW2/adivinar.py
+++ Lab05/Lab05_Davila_Flores_Mathias_PW2/adivinar.py
@@ -24,3 +24,8 @@ def juego():
 
     intentos = 5  # Número de intentos
     print(f"Palabra a adivinar: {palabra_actual}")
+
+    while intentos > 0 and "_" in palabra_actual:
+        intento = input(f"Tienes {intentos} intentos. Introduce una letra o la palabra completa: ").lower()
+    else:
+        print(f"Entrada inválida. Debes introducir una letra o una palabra de {len(palabra_original)} letras.")
