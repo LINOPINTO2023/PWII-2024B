814945f8c322901082ba521e94f70e9af8e7881e
AndersonArceValencia <aarceva@unsa.edu.pe>
2024-10-26 16:06:03 -0500

    he reutilizado el codigo y el menu y el input de poner palabras ya esta

--- Lab05/Lab05_Anderson_Arce_Valencia_Pweb2/palabraAleatoria.py
+++ Lab05/Lab05_Anderson_Arce_Valencia_Pweb2/palabraAleatoria.py
@@ -5,7 +5,8 @@ n = random.choice(words)
 num_letras = len(n)
 max_ocultas = int(num_letras * 0.6)  # Máximo 60% de letras ocultas
 indices_ocultos = random.sample(range(num_letras), max_ocultas)  # Elegimos qué letras ocultar
-
+print(max_ocultas)
+print(indices_ocultos)
 palabra_oculta = ""
 
 for i, letra in enumerate(n):
@@ -14,5 +15,13 @@ for i, letra in enumerate(n):
     else:
         palabra_oculta += letra
 
-print(palabra_oculta)
 
+
+#juegp:
+intentos = 5
+while intentos > 0 and "_" in palabra_oculta:
+    print("Palabra:"," ".join(palabra_oculta))
+    print("Intentos restantes: ", intentos,)
+    print("-----------------------------------------")
+    acertar = input("Introduce una letra o la palabra completa: " ).lower()
+    intentos = intentos - 1  
