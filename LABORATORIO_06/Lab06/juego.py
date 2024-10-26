29600c1fdfe03c2dd0eb08fee4fdcd046e746c16
AntonyTapia <atapiahu@unsa.edu.pe>
2024-10-26 17:49:48 -0500

    funcion para verificar si es una entrada correcta o incorrecta con ejmplo de uso

--- LABORATORIO_06/Lab06/juego.py
+++ LABORATORIO_06/Lab06/juego.py
@@ -3,6 +3,7 @@ import random
 #PALABRAS PARA EL JUEGO 
 PALABRAS = ["europa", "programacion", "arequipa", "ceviche", "computadora", "latinoamerica", "pisco"]
 
+#Ocultar ciertas letras de una palabra
 def ocultar(palabra):
     ocultas = int(len(palabra) * 0.6)
     indices = random.sample(range(len(palabra)), ocultas)
@@ -14,7 +15,27 @@ def ocultar(palabra):
     
     return ''.join(palabra_inicial)
 
-# Ejemplo de uso
+#Verificar si el intento es correcto o incorrecto
+def intento(palabra, entrada):
+    if(len(entrada) == 1):
+        return entrada in palabra
+    if(len(entrada) == len(palabra)):
+        return entrada == palabra
+    return False
+
+#ejemplo
 palabra_seleccionada = random.choice(PALABRAS)
-resultado = ocultar(palabra_seleccionada)
-print("Palabra oculta:", resultado)
+palabra_oculta = ocultar(palabra_seleccionada)
+    
+print("Palabra oculta:", palabra_oculta)
+
+while True:
+    entrada = input("Adivina una letra o la palabra completa: ")
+    
+    if intento(palabra_seleccionada, entrada):
+        print("¡Correcto!")
+        if entrada == palabra_seleccionada:
+            print("¡Has adivinado la palabra! Era:", palabra_seleccionada)
+            break
+    else:
+        print("Incorrecto. Intenta de nuevo.")
