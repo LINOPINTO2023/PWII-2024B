d9d2de62d02e1a529b937ce92e5c4a6a3e6a8f7b
AntonyTapia <atapiahu@unsa.edu.pe>
2024-10-26 17:15:49 -0500

    funcion para ocultar letras de un palabras, error en el retorno

--- LABORATORIO_06/Lab06/juego.py
+++ LABORATORIO_06/Lab06/juego.py
@@ -3,3 +3,17 @@ import random
 #PALABRAS PARA EL JUEGO 
 PALABRAS = {"europa", "programacion", "arequipa", "ceviche", "computadora", "latinoamerica", "pisco"}
 
+def ocultar(palabra):
+    ocultas = int(len(palabra) * 0.6)
+    indices = random.sample(range(len(palabra)), ocultas)
+    palabra_inicial = list(palabra)
+
+    for i in indices:
+        palabra_inicial[i] = "_"
+    
+    return ''.join(palabra_inicial)
+
+# Ejemplo de uso
+palabra_seleccionada = random.choice(PALABRAS)
+resultado = ocultar(palabra_seleccionada)
+print("Palabra oculta:", resultado)
