f08a5f57ec614709c62da4ce02da8e9a5252ab69
Jazmin <140028251+NaginCarferC@users.noreply.github.com>
2024-10-26 16:23:30 -0500

    lista de palabras

--- Lab05/lab05_Carfer_Carpio_Nagin_Jazmin/ahorcado.py
+++ Lab05/lab05_Carfer_Carpio_Nagin_Jazmin/ahorcado.py
@@ -1,1 +1,6 @@
-
+# -*- coding: utf-8 -*-
+import random
+#Las palabras que se adivinaran
+listapalabras = ['elefante', 'lapicero', 'hormiga', 'gallina', 'ave', 'antro', 'azul', 'piña',
+           'caracol', 'raton', 'gorila', 'peaton', 'tenedor', 'pintura', 'blanco',
+           'iguana', 'leon', 'gato', 'gallo', 'comida', 'televisor', 'papel', 'lonchera']
