edc0696c7afc68b7d40acbabaaf0b55ea10feb15
Andres <aores@unsa.edu.pe>
2024-10-25 19:22:54 -0500

    Update Tarea

--- Lab05/Lab05_Ore_Soto_Andres_Raul/tarea.py
+++ Lab05/Lab05_Ore_Soto_Andres_Raul/tarea.py
@@ -1,22 +1,20 @@
 import random
-palabras = ["Personalizado", "Jugadores", "Matematicamentu", "Teclado"]
+palabras = ["Personalizado", "Jugadores", "Paramiticuanquitaro", "Teclado"]
 word_choose = random.randint(0, len(palabras) - 1)
 
 delete_word = []
 for letra in range(len(palabras[word_choose])//2):
     random_number = random.randint(0, len(palabras[word_choose]) - 1)
-    delete_word.append(random_number)
+    if random_number not in delete_word:
+        delete_word.append(random_number)
 # Hasta aqui tengo una lista ordenada con numeros aleatorios
 delete_word = sorted(delete_word)
 
 
-for number in range(len(palabras[word_choose])):
-    new_chain = ""
-    if number in delete_word:
-        un_word = palabras[word_choose].replace(
-            palabras[word_choose][number], "_")
-        new_chain += un_word
-    else:
-        new_chain += palabras[word_choose][number]
+new_chain = list(palabras[word_choose])
 
-print(new_chain)
+for index in delete_word:
+    new_chain[index] = "_"
+
+palabra_final = "".join(new_chain)
+print(palabra_final)
