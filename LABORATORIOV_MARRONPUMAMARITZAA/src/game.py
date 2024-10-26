4302135fe3a4b19076eb6c6fea84e0b7d6e4f8eb
maritza1818 <maritzaclaudiamarronpuma@gmail.com>
2024-10-26 18:20:09 -0500

    Subir carpeta

---
+++ LABORATORIOV_MARRONPUMAMARITZAA/src/game.py
@@ -0,0 +1,17 @@
+import random
+words = ["meow", "cats", "claudita", "aea"]
+word = random.choice(words)
+guessed_letters = []
+while True:
+    letter = input("Ingrese su letra correspondiente: ")
+    guessed_letters.append(letter)
+    hidden_letter=''
+    for c in word:
+        if c in guessed_letters:
+            hidden_letter+=c
+        else:
+            hidden_letter+="_"
+    print(hidden_letter)
+    if "_" not in hidden_letter:
+        print("¡Ganaste el juego!")
+        break
