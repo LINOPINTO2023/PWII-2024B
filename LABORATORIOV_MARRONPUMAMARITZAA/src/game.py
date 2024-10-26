4ea0382940a8d9519923cb31268ac6efb1f8cf69
maritza1818 <maritzaclaudiamarronpuma@gmail.com>
2024-10-26 18:30:48 -0500

    Add attempts logic and game-over condition

--- LABORATORIOV_MARRONPUMAMARITZAA/src/game.py
+++ LABORATORIOV_MARRONPUMAMARITZAA/src/game.py
@@ -15,3 +15,21 @@ while True:
     if "_" not in hidden_letter:
         print("¡Ganaste el juego!")
         break
+attempts = 5
+
+while attempts > 0:
+    hidden_letter = ''.join([c if c in guessed_letters else '_' for c in word])
+    print("Palabra:", hidden_letter)
+    print("Intentos restantes:", attempts)
+    
+    letter = input("Ingrese su letra correspondiente: ").lower()
+    guessed_letters.append(letter)
+    if letter not in word:
+        attempts -= 1
+    
+    if "_" not in hidden_letter:
+        print("¡Ganaste el juego!")
+        break
+
+if attempts == 0:
+    print("Perdiste. La palabra era:", word)
