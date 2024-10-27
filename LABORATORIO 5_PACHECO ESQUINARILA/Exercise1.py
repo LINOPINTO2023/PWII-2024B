1ab896a375facbfcfdfe2191d9a53dae02521c10
Milene Pacheco <mpachecoe@unsa.edu.pe>
2024-10-27 00:30:29 -0500

    avance def jugar_adivinar_palabra()

--- LABORATORIO 5_PACHECO ESQUINARILA/Exercise1.py
+++ LABORATORIO 5_PACHECO ESQUINARILA/Exercise1.py
@@ -8,3 +8,25 @@ def ocultar_palabra(palabra):
     letras_a_ocultar = random.sample(range(len(palabra)), num_letras_a_ocultar)
     palabra_oculta = ''.join('_' if i in letras_a_ocultar else letra for i, letra in enumerate(palabra))
     return palabra_oculta
+
+def jugar_adivinar_palabra():
+    palabra_original = random.choice(palabras)
+    palabra_actual = ocultar_palabra(palabra_original)
+    intentos = 5  
+
+    print("Bienvenido a este juego en el debes de adivinar palabras.")
+    print("Palabra por adivinar:")
+    print(f"Palabra: {palabra_actual}")
+    print(f"Tienes {intentos} intentos.")
+
+    while intentos > 0:
+        intento = input("Introduce una letra o intenta adivinar la palabra: ")
+
+        # Si el jugador introduce una palabra completa
+        if len(intento) == len(palabra_original):
+            if intento == palabra_original:
+                print("¡BIEN HECHO! Adivinaste la palabra.")
+                return
+            else:
+                intentos -= 1
+                print("UPS, no es la palabra. Inténtalo de nuevo.")
