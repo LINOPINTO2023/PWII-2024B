8cf0268cf749edf2ea48735b46d8d857e575399a
SaulS. <ssivincham@unsa.edu.pe>
2024-10-26 21:51:10 -0500

    first logic implemented

--- Laboratorio5/SivinchaMachacaSaulAndre/juego.py
+++ Laboratorio5/SivinchaMachacaSaulAndre/juego.py
@@ -7,3 +7,19 @@ def hideWord(word):
     hiddenIndices = random.sample(range(len(word)), numHiddenLetters)
     hiddenWordList = [letter if index not in hiddenIndices else "_" for index, letter in enumerate(word)]
     return "".join(hiddenWordList), hiddenIndices
+
+def playWordGuessingGame():
+    print("Welcome to the Word Guessing Game!")
+    
+    wordList = defaultWords.copy()
+
+    chosenWord = random.choice(wordList)
+    hiddenWord, hiddenIndices = hideWord(chosenWord)
+    wordInProgress = list(hiddenWord)
+    attempts = 5  
+
+    print(f"You have {attempts} attempts to guess the word.")
+    print("Word:", hiddenWord)
+
+# Run the game
+playWordGuessingGame()
